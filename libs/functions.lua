68a5226c65a178efe3d7e8c9d1e757f634f7fdae
QuickSilver <avataraang399@gmail.com>
2019-04-28 19:24:20 +1000

    Refactor functions.lua a little
    Former-commit-id: d4b116aa99989962a9fd2afebe1852606a919990 [formerly 91a72ecff34d1da449e26f149ecdac914bfef24f]
    Former-commit-id: fed190ae9867ace05bda3c9bbddaeebb40b4997d

--- libs/functions.lua
+++ libs/functions.lua
@@ -5,11 +5,15 @@ local functions = {}
 		local height = settings.windowSizes[settings.window.selectedSize].height
 		settings.window.width = width
 		settings.window.height = height
+
 		love.window.setMode(width, height)
 		love.window.setFullscreen(settings.window.fullscreen)
-		if settings.window.fullscreen then width,height = love.window.getMode() end
-		camera = gamera.new(0,0,settings.gameWidth,settings.gameHeight)
-		camera:setWindow(0,0,width, height)
+		if settings.window.fullscreen then
+			width, height = love.window.getMode() 
+		end
+
+		camera = gamera.new(0, 0, settings.gameWidth, settings.gameHeight)
+		camera:setWindow(0, 0, width, height)
 		settings.window.cameraScale = height / settings.gameHeight
 		camera:setScale(settings.window.cameraScale)
 		settings.window.realHeight = height
@@ -19,8 +23,11 @@ local functions = {}
 	function functions.CopyTable(table)
 		local result = {}
 		for key, val in pairs(table) do
-			if type(val) == "table" then result[key] = functions.CopyTable(val)
-			else result[key] = val end
+			if type(val) == "table" then
+				result[key] = functions.CopyTable(val)
+			else
+				result[key] = val
+			end
 		end
 		return result
 	end
@@ -30,7 +37,7 @@ local functions = {}
 			if type(self[val]) == "number" then
 				return self[val]
 			elseif type(self[val]) == "table" then
-				return self[val][math.random(1,#self[val])]
+				return self[val][math.random(1, #self[val])]
 			end
 		end
 		return 0
