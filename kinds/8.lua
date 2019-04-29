5b5d3a7ad5dce80b69dddd17ceb7a8c73d4a1f13
QuickSilver <avataraang399@gmail.com>
2019-04-29 13:57:01 +1000

    Fix spacing for kind 8.lua
    Former-commit-id: 6bc946698dae319c45c93815f9626a08ec71fbc8 [formerly 8cf84db5516584d80a96707501f72b687d781db7]
    Former-commit-id: eaef853d5b1e433b8efba74cf23abf137b202d96

--- kinds/8.lua
+++ kinds/8.lua
@@ -1,15 +1,15 @@
 local kind = {}
 
-function kind:Start(attacker, itr, defender, body)
-	if (itr.purpose == 1 and ((attacker.team ~= defender.team) or attacker.team == -1 or defender.team == -1) and attacker.owner ~= defender.owner)
-	or (body.participle)
-	or (itr.purpose == 0)
-	or (itr.purpose == -1 and (attacker.team == defender.team) and (attacker.team ~= -1 or defender.team ~= -1)) then
+	function kind:Start(attacker, itr, defender, body)
+		if (itr.purpose == 1 and ((attacker.team ~= defender.team) or attacker.team == -1 or defender.team == -1) and attacker.owner ~= defender.owner)
+		or (body.participle)
+		or (itr.purpose == 0)
+		or (itr.purpose == -1 and (attacker.team == defender.team) and (attacker.team ~= -1 or defender.team ~= -1)) then
 
-		if itr.attacker_frame ~= 0 then attacker:setFrame(itr.attacker_frame) end
-		if itr.defender_frame ~= 0 then defender:setFrame(itr.defender_frame) end
-		
+			if itr.attacker_frame ~= 0 then attacker:setFrame(itr.attacker_frame) end
+			if itr.defender_frame ~= 0 then defender:setFrame(itr.defender_frame) end
+			
+		end
 	end
-end
 
 return kind
