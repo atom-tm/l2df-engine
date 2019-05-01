9b06a549fdee878a508bd2d146aeb58bcb8d42e2
QuickSilver <avataraang399@gmail.com>
2019-05-01 17:42:14 +1000

    Update conf.lua
    Former-commit-id: ed05df6e7f253c46ab1dd8d2abb71ce313a587c4 [formerly ed9892eec0fda5eeae070d3ac7fa63d04e0c1013]
    Former-commit-id: 5a3e12a2876cd864adbb5cd25f22358985c889c9

--- conf.lua
+++ conf.lua
@@ -1,8 +1,8 @@
 function love.conf(configuration)
- configuration.window.title = "Naruto: Operation Red Moon"    
+ configuration.window.title = "Naruto: Operation Red Moon"
  configuration.window.icon = nil
- configuration.window.width = 1000                                                                    
- configuration.window.height = 300
+ configuration.window.width = 640
+ configuration.window.height = 480
  configuration.window.vsync = 0
  configuration.window.msaa = 5
  configuration.window.depth = nil 
