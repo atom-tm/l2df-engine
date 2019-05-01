1ed703939f1a8b61e91d8834700898e39ec02fa5
QuickSilver <avataraang399@gmail.com>
2019-05-01 17:45:09 +1000

    Fix rooms' bugs
    Former-commit-id: 94ac78320bd77877b3c2c7d208543d87172967fd [formerly ce72736d3239edba59cff3693f5d8451f72b4d25]
    Former-commit-id: baf7d811e5bc31422ffb63934496ebb4f558e7c9

--- libs/rooms.lua
+++ libs/rooms.lua
@@ -1,13 +1,14 @@
-rooms = {}
+local rooms = { list = { } }
 
-	function rooms:initialize()
+	function rooms:init()
 		rooms.list = helper.requireAllFromFolder(settings.global.roomsFolder)
 		for key in pairs(love.handlers) do
 			local old_func = love[key] or function() end
 			love[key] = function (...)
-				old_func(...)
+				old_func(...);
+
 				if rooms.current[key] then
-					rooms.current[key](...)
+					rooms.current[key](rooms.current, ...)
 				end
 			end
 		end
@@ -15,13 +16,13 @@ rooms = {}
 	end
 
 	function rooms:set(room, input)
-		input = input or {}
-		self.current = rooms.list[tostring(room)]
-		self.current:load(input)
+		input = input or { }
+		rooms.current = rooms.list[tostring(room)]
+		rooms.current:load(input)
 	end
 
 	function rooms:reload(input)
-		self.current:load(input)
+		rooms.current:load(input)
 	end
 	
 return rooms
