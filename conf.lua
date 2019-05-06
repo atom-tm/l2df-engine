6db639c0ff4623bef30e3ea64bad524c681934ce
QuickSilver <avataraang399@gmail.com>
2019-05-06 12:00:01 +1000

    Fix spacing
    Former-commit-id: 92d73376e1ca0b053c7932100795e3f7bda3206f [formerly 03e4f56014d492c666d8018028ba6f1eb4b508ce]
    Former-commit-id: 7b2a4fb3c6cfb221eb4439a8417b734af01bbd92

--- conf.lua
+++ conf.lua
@@ -1,12 +1,12 @@
 function love.conf(configuration)
- configuration.window.title = "Naruto: Operation Red Moon"
- configuration.window.icon = nil
- configuration.window.width = 0
- configuration.window.height = 0
- configuration.window.vsync = 0
- configuration.window.msaa = 0
- configuration.window.depth = nil 
- configuration.window.resizable = true
+	configuration.window.title = "Naruto: Operation Red Moon"
+	configuration.window.icon = nil
+	configuration.window.width = 0
+	configuration.window.height = 0
+	configuration.window.vsync = 0
+	configuration.window.msaa = 0
+	configuration.window.depth = nil 
+	configuration.window.resizable = true
 
- configuration.modules.physics = false
+	configuration.modules.physics = false
 end
