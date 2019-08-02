4b43c3eca531c1cf13337d0fb28a4993787ccdce
Andrew <kd.feniks@gmail.com>
2019-08-02 10:49:27 +0300

    Images module rework
    Former-commit-id: 18fedd8c7a3a41d45a8c3a120f0394cfb66f7e97 [formerly 34c0ab63a83083c6fc23206ed6a42eca65b50445]
    Former-commit-id: 952f45f66edb618c0f9fdb4151b12e821633723d

--- src/images.lua
+++ src/images.lua
@@ -2,61 +2,109 @@ local core = l2df or require((...):match("(.-)[^%.]+$") .. "core")
 assert(type(core) == "table" and core.version >= 1.0, "Images works only with l2df v1.0 and higher")
 assert(type(love) == "table", "Images works only under love2d environment")
 
-local images = { list = { } }
+local fs = love.filesystem
+local notNil = core.import("helper").notNil
 
-	function images.Load(filepath, cutting_info , filter)
-		for i in pairs(images.list) do
-			if images.list[i].path == filepath then
-				return images.list[i]
-			end
+--- Loads an image into an array
+local function loadResourse(filepath, global, linear, mipmaps, wrap)
+	local index = filepath
+
+	local resourse = images.list.global[index] or images.list.temporary[index] or nil
+	if resourse then return resourse end
+
+	local storage = global and images.list.global or images.list.temporary
+	storage[index] = love.graphics.newImage(filename, {linear = linear, mipmaps = mipmaps})
+	resourse = storage[index]
+	if resourse then
+		resourse:setWrap(wrap,wrap)
+		return resourse
+	end
+
+	return nil
+end
+
+--- Adds a frame
+local function addQuad(self, x, y, w, h, id)
+	local quad = love.graphics.newQuad(x, y, w, h, self.info.width, self.info.height)
+	id = id or #self.quads
+	self.quads[id] = quad
+	self.info.frames = self.info.frames + 1
+end
+
+local images = { list = { global = {}, temporary = {} }, load, draw }
+
+	--- Clearing an array of temporary resources to free up memory
+	function images.clear()
+		for key in pairs(images.list.temporary) do
+			images.list.temporary[key] = nil
 		end
-		local image = {
-			image = love.graphics.newImage(filepath, {linear = true, mipmaps = true} ),
-			sprites = {},
-			path = filepath
-		}
-		image.image:setWrap("clampzero", "clampzero")
-		if type(cutting_info) == "table" then
-			cutting_info.x = cutting_info.x or 1
-			cutting_info.y = cutting_info.y or 1
-			cutting_info.mx = cutting_info.mx or 0
-			cutting_info.my = cutting_info.my or 0
-			for i = 0, cutting_info.y - 1 do
-				for j = 0, cutting_info.x - 1 do
-					quad = love.graphics.newQuad(cutting_info.w*j + cutting_info.mx, cutting_info.h*i + cutting_info.my ,cutting_info.w,cutting_info.h, image.image:getDimensions())
-					table.insert(image.sprites, quad)
-				end
-			end
-			image.w = cutting_info.w
-			image.h = cutting_info.h
+		images.list.temporary = {}
+	end
+
+	--- Loads an image and returns the object of that image
+	function images.load(filepath, cutting_info, privacy, properties)
+
+		if not (filepath and fs.getRealDirectory(filepath)) then return end
+
+		local image = { resourse, batch, quads = {}, info = {} }
+		local w, h, x, y
+		local x_offset, y_offset = 0, 0
+		local linear, mipmaps, wrap, global = false, true, "clampzero", true
+		local info = {width = 0, height = 0, frames = 1}
+
+		if type(properties) == "table" then
+			linear = notNil(properties.linear, linear)
+			mipmaps = notNil(properties.mipmaps, mipmaps)
+			wrap = notNil(properties.wrap, wrap)
+			global = notNil(properties.global, global)
 		else
-			image.w, image.h = image.image:getDimensions()
+			linear = notNil(properties, linear)
 		end
-		if filter then
-			image.image:setFilter(filter, filter)
+
+		image.resourse = loadResourse(filepath, global, linear, mipmaps, wrap)
+		if not image.resourse then return end
+
+		cutting_info = type(cutting_info) == "table" and cutting_info or {}
+		w = cutting_info.w or cutting_info[1] or info.width
+		h = cutting_info.h or cutting_info[2] or info.height
+		x = cutting_info.x or cutting_info[3] or 1
+		y = cutting_info.y or cutting_info[4] or 1
+		x_offset = cutting_info.x_offset or cutting_info[5] or 0
+		y_offset = cutting_info.y_offset or cutting_info[6] or 0
+
+		info.width = image.resourse:getWidth()
+		info.height = image.resourse:getHeight()
+		info.frames = x * y
+
+		image.batch = info.frames > 1 and love.graphics.newSpriteBatch(image.resourse,1000)
+		image.info = info
+		image.addQuad = addQuad
+		image.draw = images.draw
+
+		for j = 0, y - 1 do
+			for i = 0, x - 1 do
+				image:addQuad(i * w + x_offset, j * h + y_offset, w, h)
+			end
 		end
-		table.insert(images.list, image)
-		image.setQuad = images.setQuad
+
 		return image
 	end
 
-	function images:setQuad(id, x, y, w, h)
-		if id ~= nil and self.sprites[id] ~= nil then
-			self.sprites[id]:setViewport(x,y,w,h)
-			return id
-		else
-			local quad = love.graphics.newQuad(x,y,w,h, self.image:getDimensions())
-			if id == nil then
-				self.sprites[#self.sprites + 1] = quad
-				return #self.sprites + 1
-			else
-				self.sprites[id] = quad
-				return id
-			end
-		end
+
+	function images.draw(self, x, y)
+
+		if not self then return end
+		local sprite = self.batch or self.resourse
+		if not sprite then return end
+
+		x = x or 0
+		y = y or 0
+
+		love.graphics.draw(sprite, x, y)
+
 	end
 
-	function images.draw(image, sprite, x, y, facing, size, r,g,b,a, other)
+	--[[function images.draw(image, sprite, x, y, facing, size, r,g,b,a, other)
 		if size == nil then size = 1 end
 		if facing == 0 or facing == nil then facing = 1 end
 		local width = 1
@@ -81,6 +129,7 @@ local images = { list = { } }
 			love.graphics.draw(image.image,x,y,other.r,width * facing,height,other.ox,other.oy,other.kx,other.ky)
 		end
 		love.graphics.setColor(ro, go, bo, ao)
-	end
+	end]]
 
 return images
+
