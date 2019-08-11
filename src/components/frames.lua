8976ef277a067227e9ebb6cad5448f754751c45d
QuickSilver <avataraang399@gmail.com>
2019-08-12 01:21:29 +1000

    Add FramesComponent
    Former-commit-id: 48954ddec48ffdabb6334385b9c75364c88a9cbe [formerly d51c790e1da568acde2e9dfce87284f1b3de69fa]
    Former-commit-id: adfaf47498d3e759b602daa29001db29c021faaa

--- src/components/frames.lua
+++ src/components/frames.lua
@@ -2,6 +2,7 @@ local core = l2df or require((...):match("(.-)[^%.]+%.[^%.]+$") or "" .. "core")
 assert(type(core) == "table" and core.version >= 1.0, "FramesComponent works only with l2df v1.0 and higher")
 
 local Component = core.import "core.entities.component"
+local Media = core.import "media"
 
 local random = math.random
 local type = _G.type
@@ -14,9 +15,35 @@ local FramesComponent = Component:extend({
 	next_frame = 0,
 	head = {
 		default_idle = "idle",
-		default_air = "air_idle"
-	}
+		default_air = "air_idle",
+		sprites = { }
+	},
+	frame_timer = 0
 })
+
+	--- Init component, collect all pics
+	function FramesComponent:init(entity)
+		self.pics = { }
+		if entity.head and entity.head.sprites then
+			for i = 1, #entity.head.sprites do
+				local sprite = entity.head.sprites[i]
+				local info = {
+					w = sprite.w,
+					h = sprite.h,
+					x = sprite.row,
+					y = sprite.col,
+					x_offset = sprite.x_offset,
+					y_offset = sprite.y_offset
+				}
+				local img = Media.Image(sprite.file, info)
+				local size = #self.pics
+				for j = 1, img.info.frames do
+					self.pics[size + j] = { img, j }
+				end
+			end
+		end
+		self:super(entity)
+	end
 
 	--- Set current frame
 	-- @param key, string|number  Frame key
@@ -50,6 +77,23 @@ local FramesComponent = Component:extend({
 			end
 		end
 		return false
+	end
+
+	function FramesComponent:update(dt)
+		if self.frame then
+			self.frame_timer = self.frame_timer + dt * 1000
+			local no_wait = not self.wait or self.wait == 0
+			if no_wait or self.frame_timer > self.wait then
+				self.frame_timer = self.frame_timer - (no_wait and self.frame_timer or self.wait)
+				self:setFrame(self.next_frame)
+			end
+		end
+	end
+
+	function FramesComponent:draw()
+		if not self.frame then return end
+		local pic = self.pics[self.frame.pic]
+		pic[1]:draw(self.x, self.y, pic[2])
 	end
 
 return FramesComponent
