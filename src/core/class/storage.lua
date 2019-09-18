3a4f5cbb70787e052056d82ddddb165e414ecd17
Andrew <kd.feniks@gmail.com>
2019-09-19 00:22:32 +0300

    Add in-memory Storage
    Former-commit-id: 80f48a16e320a3991f19c0d18cd4912f26f249da [formerly 6b4e9572e4d843ca8e2607acf21a219142842ac5]
    Former-commit-id: caa7d273ba96cbbf3e418e8d6ffca4c4f60a76f2

---
+++ src/core/class/storage.lua
@@ -0,0 +1,80 @@
+local core = l2df or require((...):match("(.-)core.+$") or "" .. "core")
+assert(type(core) == "table" and core.version >= 1.0, "Entities works only with l2df v1.0 and higher")
+
+local Class = core.import "core.class"
+
+local Storage = Class:extend()
+
+	--- Storage initialization
+	function Storage:init()
+		self.list = {}
+		self.map = {}
+		self.free = {}
+		self.length = 0
+	end
+
+	--- Add new object to storage
+	--  @param object
+	function Storage:add(object)
+		local id = self:has(object)
+		if id then return id end
+
+		if #self.free > 0 then
+			id = self.free[#self.free]
+			self.free[#self.free] = nil
+		else
+			self.length = self.length + 1
+			id = self.length
+		end
+
+		self.list[id] = object
+		self.map[object] = id
+		return id
+	end
+
+
+	--- Remove object from storage
+	--  @param object
+	function Storage:remove(object)
+		local id = self.map[object]
+		if not id then return false end
+		self.list[id] = nil
+		self.map[object] = nil
+		self.free[#self.free + 1] = id
+		self.length = id == self.length and self.length - 1 or self.length
+		return true
+	end
+
+	--- Remove object from storage by Id
+	--  @param id, number
+	function Storage:removeById(id)
+		if not self.list[id] then return false end
+		self.map[self.list[id]] = nil
+		self.list[id] = nil
+		self.free[#self.free + 1] = id
+		return true
+	end
+
+	--- Return object from storage by Id
+	function Storage:getById(id)
+		return self.list[id] or false
+	end
+
+	--- Checks for object in storage
+	function Storage:has(object)
+		return self.map[object] or false
+	end
+
+	function Storage:enum()
+		local id = 0
+		return function ()
+			if id < self.length then
+				id = id + 1
+				return id, self.list[id]
+			else
+				return nil
+			end
+		end
+	end
+
+return Storage
