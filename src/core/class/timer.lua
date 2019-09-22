8ca56b16d9c13e6d2bbf59667f8edcf27d9d3394
Andrew <kd.feniks@gmail.com>
2019-09-23 02:38:04 +1000

    Add Timer class
    Former-commit-id: 6b7d09006d8660e722fce19efa6c9c748cb66ebc [formerly b485ec4d5f45a606d641a96e8660f35b220f7c7e]
    Former-commit-id: 00f2fa9c62875fec713d93468510e3dcca6e8fec

---
+++ src/core/class/timer.lua
@@ -0,0 +1,23 @@
+local core = l2df or require((...):match("(.-)core.+$") or "" .. "core")
+assert(type(core) == "table" and core.version >= 1.0, "Entities works only with l2df v1.0 and higher")
+
+local Class = core.import "core.class"
+local EventManager = core.import "core.manager.event"
+
+local Timer = Class:extend()
+
+    function Timer:init(tick)
+        self.i = 0
+        self.tick = tick
+        EventManager:subscribe("update", self.update, nil, self)
+    end
+
+    function Timer:update()
+        self.i = self.i + 1
+        if self.i >= self.tick then
+            EventManager:invoke("timer", self)
+            self.i = 0
+        end
+    end
+
+return Timer
