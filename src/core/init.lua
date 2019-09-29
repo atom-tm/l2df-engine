d6c6c336655ccf04adee080378c2689dc3571ac8
QuickSilver <avataraang399@gmail.com>
2019-09-30 05:07:31 +1000

    Update core, add feature for setting engine root

--- src/core/init.lua
+++ src/core/init.lua
@@ -1,9 +1,40 @@
 local __DIR__ = (...):match("(.-)[^%.]+$")
 
-local core = { version = 1.0, nodes = {} }
+local fs = love and love.filesystem
+local strmatch = string.match
+local strgsub = string.gsub
+local strsub = string.sub
+
+local source = nil
+local gsource = nil
+
+local core = { version = 1.0 }
 
 	function core.import(name)
 		return require(__DIR__ .. name)
+	end
+
+	function core.root()
+		source = source or core.fullpath('', 3)
+		gsource = gsource or source and ('^' .. source)
+		return source
+	end
+
+	function core.modulepath(path)
+		return strgsub(strgsub(path, source, __DIR__), '/', '.')
+	end
+
+	function core.fullpath(path, depth, info)
+		if fs then
+			info = info or debug.getinfo(depth or 2, 'Sn')
+			local prefix = info.source
+				:sub(2, -1)
+				:gsub(fs.getSource() .. '/?', '')
+				:gsub(info.name .. '.lua', '')
+
+			return prefix .. path
+		end
+		return path
 	end
 
 return core
